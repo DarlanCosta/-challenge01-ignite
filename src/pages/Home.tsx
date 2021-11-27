20502f2dfe0d430d2e31d4c7c1ea35b94457ede9
Darlan Costa <darlan.costa@mazzafc.tech>
2021-11-27 11:42:23 -0300

    final commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,15 +9,41 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, newTask])
+
+
     //TODO - add new task
+    // const taskWithSameTitle = tasks.find(task => task.title === newTaskTitle);
+
+    // if (taskWithSameTitle) {
+    //   // return Alert.
+    // }
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }))
+
+    const foundItem = updatedTasks.find(item => item.id = id)
+
+    if (!foundItem) return;
+
+    foundItem.done = !foundItem.done;
+
+    setTasks(updatedTasks) ;
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const updatedTask = tasks.filter(task => task.id !== id);
+
+    setTasks(updatedTask);
   }
 
   return (
@@ -31,6 +57,8 @@ export function Home() {
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
       />
+
+      <Text onPress={() => console.warn(tasks)}> OLOCO</Text>
     </View>
   )
 }
